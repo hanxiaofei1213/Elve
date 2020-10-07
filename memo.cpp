897f1c1b46a511f6f09c1bc36ec01e6f1ba2342b
lastpast <1813553862@qq.com>
2020-10-07 16:49:02 +0800

    重构部分memo代码

--- memo.cpp
+++ memo.cpp
@@ -1,43 +1,63 @@
-#include <QPixmap>
-#include <QSound>
+
+
 
 #include "memo.h"
 
 
 // 构造函数
-// TODO：搞一下背景
 Memo::Memo(QWidget* parent) : QWidget(parent)
 {
 	// 初始化属性
+	m_pixmap = new QPixmap;
 	m_mainLabel = new QLabel(this);
-	m_checkBoxList = new QList<QCheckBox *>;
-	m_mainlayout = new QVBoxLayout(m_mainLabel);
-
-	m_mainLabel->setFixedSize(150, 140);
-	
+	m_checkBoxMap = new QMap<QString, QCheckBox*>;
+	m_size = new QSize(150, 140);
+	m_pageCount = 1;
+	m_nowPage = 1;
 
 	// 设置背景
-	QPixmap pix(":/myElve/usePic/memoBackground.png");
-	m_mainLabel->setPixmap(pix);
-	addCheckBoxSlot("for test");
-	
+	setBackgroundPic(":/myElve/usePic/memoBackground.png");
+
+	// 设置label大小
+	setLabelSize(*m_size);
 }
 
 // 析构函数
 Memo::~Memo()
 {
-	delete m_checkBoxList;
+	qDebug() << "xigou";
+	delete m_checkBoxMap;
 }
+
+/**
+ * 函数名：setBackgroundPic
+ * 作用：设置整个备忘录的背景图片
+ */
+void Memo::setBackgroundPic(const QString& a_picName)
+{
+	// 检查图片是否合法
+	if (a_picName == nullptr || a_picName.isEmpty() || !m_pixmap->load(a_picName))
+	{
+		QMessageBox::warning(this, "Programe Error", " Illegal picture in Memo::setBackgroundPic(const QString&) ");
+		exit(0);
+	}
+
+	// 加载图片成功
+	m_mainLabel->setPixmap(*m_pixmap);
+}
+
+
+void Memo::setLabelSize(const QSize& a_size)
+{
+	m_mainLabel->setFixedSize(a_size);
+}
+
 
 // TODO：真的要每次都设一下布局吗
 // 添加checkBox的方法，最多四个现在
 void Memo::addCheckBoxSlot(QString a_text)
 {
-	QCheckBox* box = new QCheckBox(a_text, m_mainLabel);
-	m_checkBoxList->append(box);
-	connect(box, &QCheckBox::stateChanged, this, &Memo::addThoughtLineSlot);
-	m_mainlayout->addWidget(box);
-	m_mainLabel->setLayout(m_mainlayout);
+
 }
 
 // 删除checkBox的方法
--- memo.h
+++ memo.h
@@ -5,10 +5,14 @@
 
 #pragma once
 #include <QWidget>
-#include <QList>
+#include <QMap>
 #include <QCheckBox>
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QSize>
+#include <QPixmap>
+#include <QDebug>
+#include <QMessageBox>
 
 class Memo : public QWidget {
 	Q_OBJECT
@@ -16,14 +20,25 @@ public:
 	Memo(QWidget* parent = 0);
 	~Memo();
 
-	QLabel* m_mainLabel;
-	QList<QCheckBox*>* m_checkBoxList;   // 存放复选框的数组
-	QVBoxLayout* m_mainlayout;
+	void setBackgroundPic(const QString&);       // 设置背景图片
+	void setLabelSize(const QSize&);             // 设置label的大小   
+	void setLocation(const QSize&);              // 设置部件的位置
+	void showOnePageBox();                       // 展示一页复选框
+
+
+	QPixmap* m_pixmap;                           // 存放图片
+	QLabel* m_mainLabel;                         // 显示图片的label
+	QMap<QString, QCheckBox*>* m_checkBoxMap;    // 存放复选框的容器
+	QSize* m_size;                               // 控件大小
+	QSize* m_location;                           // 部件的位置
+	const int m_onePageBoxCount = 4;             // 一页展示多少个待做事件
+	int m_pageCount;                             // 一共要多少页
+	int m_nowPage;                               // 当前第几页
 
 
 public slots:
 	void addThoughtLineSlot(int);
-	void addCheckBoxSlot(QString);
-	void deleteCheckBoxSlot();
+	void addCheckBoxSlot(QString);               // 添加复选框 
+	void deleteCheckBoxSlot();                   // 删除复选框
 
 };
